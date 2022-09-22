1a60b450057bf073b82c925079a2be62d4ad35cf
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2022-09-22 08:16:08 -0700

    done with button

--- Parser/Form1.Designer.cs
+++ Parser/Form1.Designer.cs
@@ -50,11 +50,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(197, 497);
+            this.button1.Location = new System.Drawing.Point(183, 498);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(105, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "ChooseFile";
+            this.button1.Text = "Choose File";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
